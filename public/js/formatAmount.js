baf4f3406159092b8c5de22e76ea75b2e486d0f4
Rodrigo <rfnobando@gmail.com>
2024-03-14 22:08:16 -0300

    Changed some lines from formatAmount.js

--- public/js/formatAmount.js
+++ public/js/formatAmount.js
@@ -1,37 +1,26 @@
-// Función para reformatear un número con coma a uno con punto como separador decimal
-function reformatearNumero (numero) {
-    // Eliminar cualquier carácter que no sea un número o un punto
-    var numeroReformateado = numero.replace(/[^0-9.]/g, '');
+function formatNumber (number) {
+    let formattedNumber = number.replace(/[^0-9.]/g, '');
 
-    // Limitar la cantidad de decimales a dos
-    var partes = numeroReformateado.split('.');
-    if (partes.length > 1) {
-        partes[1] = partes[1].slice(0, 2); // Limitar a dos decimales
-        numeroReformateado = partes.join('.');
+    let parts = formattedNumber.split('.');
+    if (parts.length > 1) {
+        parts[1] = parts[1].slice(0, 2);
+        formattedNumber = parts.join('.');
     }
 
-    // Asegurarse de que solo hay un punto como separador decimal
-    var puntos = numeroReformateado.split('.').length - 1;
-    if (puntos > 1) {
-        // Si hay más de un punto, eliminar los puntos adicionales
-        numeroReformateado = numeroReformateado.replace(/\./g, (match, offset, string) => {
+    const points = formattedNumber.split('.').length - 1;
+    if (points > 1) {
+        formattedNumber = formattedNumber.replace(/\./g, (match, offset, string) => {
             return offset ? '' : match;
         });
     }
-    return numeroReformateado;
+
+    return formattedNumber;
 }
 
-// Obtener el input
-var inputNumero = document.getElementById('amount');
+const amountInput = document.getElementById('amount');
 
-// Agregar un listener para el evento input
-inputNumero.addEventListener('input', (event) => {
-    // Obtener el valor actual del input
-    var valor = event.target.value;
-
-    // Reformatear el número
-    var numeroReformateado = reformatearNumero(valor);
-
-    // Actualizar el valor del input con el número reformateado
-    event.target.value = numeroReformateado;
+amountInput.addEventListener('input', (event) => {
+    const valor = event.target.value;
+    const formattedNumber = formatNumber(valor);
+    event.target.value = formattedNumber;
 });
